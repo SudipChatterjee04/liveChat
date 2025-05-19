1538e0b1195c1185a0c61160fe01db633b876a06
LakShyamCoaching <cse.l20211070@gmail.com>
2025-05-19 23:26:19 +0530

    done

---
+++ src/main/java/com/goldencat/chatroom/model/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.goldencat.chatroom.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+
+    private String username;
+
+    private String content;
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
